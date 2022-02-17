e54d5f8082732652e35643e9a903e708b6835e00
Monkey <florentheyraud.pro@gmail.com>
2022-02-17 19:39:03 +0100

    delete useless hamster, add cat fighter sprite, add animation idle

--- src/App.js
+++ src/App.js
@@ -7,29 +7,56 @@ const App = () => {
   const [initialize, setInitialize] = useState(false);
   const [game, setGame] = useState(null);
 
-  let hamster;
   let cursors;
+  let catFighter;
 
   function preload() {
-    this.load.image("hamster", "/sprites/hamster.png");
+    this.load.spritesheet("catFighter", "/sprites/catFighter.png", {
+      frameWidth: 64,
+      frameHeight: 64,
+    });
   }
 
   function create() {
-    hamster = this.physics.add.image(200, 400, "hamster");
-    hamster.body.collideWorldBounds = true;
+    catFighter = this.physics.add.sprite(600, 370, "catFighter");
+    catFighter.body.collideWorldBounds = true;
+    catFighter.setScale(4);
+
     cursors = this.input.keyboard.createCursorKeys();
+
+    this.anims.create({
+      key: "idle",
+      frames: this.anims.generateFrameNumbers("catFighter", {
+        frames: [0, 1, 2, 3],
+      }),
+      frameRate: 12,
+      repeat: -1,
+    });
+
+    this.anims.create({
+      key: "walk",
+      frames: this.anims.generateFrameNumbers("catFighter", {
+        frames: [0, 1, 2, 3, 4, 5, 6, 7, 8],
+      }),
+      frameRate: 12,
+      repeat: -1,
+    });
+
+    catFighter.play("idle");
   }
 
   function update() {
-    hamster.setVelocityX(0);
-    if (cursors.up.isDown) {
-      hamster.setVelocity(0, -300);
+    catFighter.setVelocityX(0);
+    const onFloor = catFighter.body.onFloor();
+
+    if (onFloor && cursors.up.isDown) {
+      catFighter.setVelocity(0, -300);
     }
-    if (cursors.left.isDown) {
-      hamster.setVelocity(-100, 0);
+    if (onFloor && cursors.left.isDown) {
+      catFighter.setVelocity(-300, 0);
     }
-    if (cursors.right.isDown) {
-      hamster.setVelocity(100, 0);
+    if (onFloor && cursors.right.isDown) {
+      catFighter.setVelocity(300, 0);
     }
   }
 
