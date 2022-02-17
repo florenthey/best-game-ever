42722f27f855a32f39b3dfbe0d0ef262189a6a87
Monkey <florentheyraud.pro@gmail.com>
2022-02-17 01:54:08 +0100

    Add phaser 3, add cute hamster and keyboard event

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Phaser from "phaser";
+import { IonPhaser } from "@ion-phaser/react";
+import { useEffect, useState } from "react";
 
-function App() {
+const App = () => {
+  const [initialize, setInitialize] = useState(false);
+  const [game, setGame] = useState(null);
+
+  let hamster;
+  let cursors;
+
+  // Je set mes assets
+  function preload() {
+    this.load.image("hamster", "/sprites/hamster.png");
+  }
+
+  // J'ajoute des propriétés et évènements à mes objets'
+  function create() {
+    hamster = this.physics.add.image(200, 400, "hamster");
+    hamster.body.collideWorldBounds = true;
+    cursors = this.input.keyboard.createCursorKeys();
+  }
+  console.log("cursors", cursors);
+
+  // Intéractions
+  function update() {
+    hamster.setVelocityX(0);
+    if (cursors.up.isDown) {
+      console.log("cursors");
+      hamster.setVelocity(0, -300);
+    }
+    if (cursors.left.isDown) {
+      console.log("cursors");
+      hamster.setVelocity(-100, 0);
+    }
+    if (cursors.right.isDown) {
+      console.log("cursors");
+      hamster.setVelocity(100, 0);
+    }
+  }
+
+  console.log("GAME", game);
+
+  const config = {
+    width: "100%",
+    height: 550,
+    type: Phaser.AUTO,
+    physics: { default: "arcade", arcade: { gravity: { y: 450 } } },
+    scene: { preload, create, update },
+  };
+
+  useEffect(() => {
+    if (initialize === false) {
+      setInitialize(true);
+      setGame(config);
+    }
+  });
+
+  console.log(game);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Phaser test</h1>
+      <IonPhaser game={game} initialize={initialize} />
     </div>
   );
-}
+};
 
 export default App;
