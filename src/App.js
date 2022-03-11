cfe98d2d0f18699f433d630c2518fff8b0a422a1
Monkey <florentheyraud.pro@gmail.com>
2022-03-11 16:51:20 +0100

    add progress bar

--- src/App.js
+++ src/App.js
@@ -216,6 +216,46 @@ const App = () => {
 
   const bootScene = {
     preload: function () {
+      var progressBar = this.add.graphics();
+      var progressBox = this.add.graphics();
+      progressBox.fillStyle(0x222222, 0.8);
+      progressBox.fillRect(240, 270, 320, 50);
+
+      var width = this.cameras.main.width;
+      var height = this.cameras.main.height;
+      var loadingText = this.make.text({
+        x: width / 2,
+        y: height / 2 - 50,
+        text: "Loading...",
+        style: {
+          font: "20px monospace",
+          fill: "#ffffff",
+        },
+      });
+      loadingText.setOrigin(0.5, 0.5);
+
+      var percentText = this.make.text({
+        x: width / 2,
+        y: height / 2 - 5,
+        text: "0%",
+        style: {
+          font: "18px monospace",
+          fill: "#ffffff",
+        },
+      });
+      percentText.setOrigin(0.5, 0.5);
+
+      var assetText = this.make.text({
+        x: width / 2,
+        y: height / 2 + 50,
+        text: "",
+        style: {
+          font: "18px monospace",
+          fill: "#ffffff",
+        },
+      });
+      assetText.setOrigin(0.5, 0.5);
+
       this.load.audio("music", "assets/audio/music.ogg");
       this.load.audio("plouf", "assets/audio/plouf.ogg");
       this.load.audio("cling", "assets/audio/cling.ogg");
@@ -228,6 +268,29 @@ const App = () => {
         frameHeight: 48,
       });
       this.load.image("lava", "assets/lava.png");
+
+      for (var i = 0; i < 500; i++) {
+        this.load.audio("music" + i, "music.ogg");
+      }
+      this.load.on("progress", function (value) {
+        console.log(value);
+        progressBar.clear();
+        progressBar.fillStyle(0xffffff, 1);
+        progressBar.fillRect(250, 280, 300 * value, 30);
+        percentText.setText(parseInt(value * 100) + "%");
+      });
+
+      this.load.on("fileprogress", function (file) {
+        console.log(file.src);
+        assetText.setText("Loading asset: " + file.src);
+      });
+      this.load.on("complete", function () {
+        progressBar.destroy();
+        loadingText.destroy();
+        percentText.destroy();
+        assetText.destroy();
+        console.log("complete");
+      });
     },
 
     create: function () {
